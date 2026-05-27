c493279ffe118948fc9d21efd274665cc76d0b54
uie76947 <minzi.zhang@aumovio.com>
2026-05-27 18:05:14 +0800

    2026.5.27 for thread

--- Foundation/Thread/include/Thread.h
+++ Foundation/Thread/include/Thread.h
@@ -4,10 +4,15 @@
 
 #include "Foundation.h"
 #include <sched.h>
+#include <cstdint>
+#include <string>
+#include <pthread.h>
+#include <memory>
+#include <mutex>
 
 namespace Poco {
 
-class Foundation_API ThreadImpl
+class Foundation_API Thread
 {
 public:
     enum Priority
@@ -24,10 +29,66 @@ public:
         POLICY_DEFAULT = SCHED_OTHER
     };
 
-    ThreadImpl();
-    ~ThreadImpl();
+    /* Creates a thread. Call start() to start it.
+     *  The optional sigMask parameter specifies which signals should be blocked.
+     *  To block a specific signal, set the corresponding bit in the sigMask.
+     *  Multiple bits can be set in the mask to block multiple signals if needed.
+     */
+    Thread(uint32_t sigMask = 0);
+
+    /* Creates a thread. Call start() to start it.
+     *  The optional sigMask parameter specifies which signals should be blocked.
+     *  To block a specific signal, set the corresponding bit in the sigMask.
+     *  Multiple bits can be set in the mask to block multiple signals if needed.
+     */
+    Thread(const std::string& name, uint32_t sigMask = 0);
+
+
+    ~Thread();
+
+    /* Returns the unique thread ID of the thread */
+    int id() const;
+
+    /* Returns the native thread ID of the thread */
+    pthread_t tid() const;
+
+    /* Returns the name of the thread */
+    std::string getName() const;
+
+
+private:
+
+    struct ThreadData
+    {
+        ThreadData(): thread{}
+        {}
+
+        pthread_t thread;
+        std::string name;
+    };
+    
+    std::unique_ptr<ThreadData> _pData;
+    int _id;
+
 
 };
+
+inline int Thread::id() const
+{
+    return _id;
 }
+
+inline pthread_t Thread::tid() const
+{
+    return _pData->thread;
+}
+
+inline std::string Thread::getName() const
+{
+    return _pData->name;
+}
+
+
+} // namespace Poco
 
 #endif // Foundation_Thread_Thread_INCLUDED
---
+++ Foundation/Thread/src/Thread.cpp
@@ -0,0 +1,9 @@
+#include "Thread.h"
+
+namespace Poco {
+
+    Thread::Thread(uint32_t sigMask)
+    {
+        // Constructor implementation
+    }
+} // namespace Poco
